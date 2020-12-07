32000c11dc2440a8a2747ced35620b0e68cc3400
Stephen Kilbourn <stephen@dialexa.com>
2020-12-06 21:57:51 -0600

    day 6 part 2

--- src/6/index.js
+++ src/6/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-import { addGroups } from './main';
+import { addGroups, addUniqueGroups, answeredByAll } from './main';
 
 const input = fs
   .readFileSync(path.join(__dirname, 'input.txt'))
@@ -9,3 +9,5 @@ const input = fs
   .split('\n\n');
 
 console.log('Part 1: add groups', addGroups(input));
+
+console.log('Part 2: add answers of groups', addUniqueGroups(input));
--- src/6/main.js
+++ src/6/main.js
@@ -14,14 +14,38 @@ const countUnique = (string) => {
   const distinctAnswers = [...new Set(string)];
   return distinctAnswers.length;
 };
-// strip new line from string
 // count total for each group
 const addGroups = (arrayOfGroups) => {
   let count = 0;
   arrayOfGroups.forEach((group) => {
-    count = count + countUnique(removeNewLines(group));  });
+    count = count + countUnique(removeNewLines(group));
+  });
   return count;
 };
 
+const answeredByAll = (string) => {
+  let stringArray = string.toString().split('\n');
+  let uniqueAnswers = {};
+  stringArray.forEach((person) => {
+    for (let answer of person) {
+      uniqueAnswers[answer] = uniqueAnswers[answer] + 1 || 1;
+    }
+  });
+  let count = [];
+  for (let key of Object.keys(uniqueAnswers)) {
+    if (uniqueAnswers[key] === stringArray.length) {
+      count.push(key);
+    }
+  }
+  return count.length;
+};
 
-export { addGroups}
+const addUniqueGroups = (arrayOfGroups) => {
+  let count = 0;
+  arrayOfGroups.forEach((group) => {
+    count = count + answeredByAll(group);
+  });
+  return count;
+};
+
+export { addGroups, addUniqueGroups, answeredByAll };
