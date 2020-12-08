1d5f8a4cc9d656d87961bed2abc638a95a9b471c
Stephen Kilbourn <stephen@dialexa.com>
2020-12-08 10:52:43 -0600

    cleanup

--- src/7/main.js
+++ src/7/main.js
@@ -12,11 +12,11 @@ const testRuleArray = [
 
 class LuggageTree {
   constructor(rule_array) {
-    this.bags_lookup = this.parseToBagsLookup(rule_array);
-    this.rules = this.parseToRulesLookup(rule_array);
+    this.bags_lookup = this.bagsLookup(rule_array);
+    this.rules = this.rulesLookup(rule_array);
   }
 
-  parseToBagsLookup(rule_array) {
+  bagsLookup(rule_array) {
     const regex = new RegExp(/(\d+) (\w+ \w+)/);
     let bags_lookup = {};
     for (let rule of rule_array) {
@@ -29,8 +29,7 @@ class LuggageTree {
       }
       children = children.split(',');
       for (let child of children) {
-        
-        child = child.replace(/\sbags?/g, '')
+        child = child.replace(/\sbags?/g, '');
         let [, count, name] = regex.exec(child);
         count = parseInt(count);
         let bag = bags_lookup[name];
@@ -44,7 +43,7 @@ class LuggageTree {
     return bags_lookup;
   }
 
-  parseToRulesLookup(rule_array) {
+  rulesLookup(rule_array) {
     let bags_lookup = {};
     const regex = new RegExp(/(\d+) (\w+ \w+)/);
 
@@ -72,8 +71,7 @@ class LuggageTree {
     let childrenCount = 0;
     for (let bag of rules) {
       let { name, count } = bag;
-      childrenCount += count;
-      childrenCount += count * this.countChildrenInBag(name);
+      childrenCount += count + count * this.countChildrenInBag(name);
     }
     return childrenCount;
   }
@@ -119,6 +117,7 @@ let shiney_gold = luggage.bags_lookup['shiny gold'];
 console.log('Part One: count of shiney gold', shiney_gold.countUniqueParents());
 
 let shiny_child_count = luggage.countChildrenInBag('shiny gold');
-console.log('Part Two: Total number of bags in a shiney gold', shiny_child_count);
-
-
+console.log(
+  'Part Two: Total number of bags in a shiney gold',
+  shiny_child_count
+);
